2f2fa9d9b186e3ab9359cafc3e1949383c0e9380
Lucas Oliveira <lucasoliveira.dpb@gmail.com>
2024-10-01 11:39:03 -0300

    adding post-datails

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home_page),
-    path("posts", views.posts),
-    path("detalhes", views.post_datails)
+    path("", views.home_page, name="starting-page"),
+    path("posts", views.posts, name="posts-page"),
+    path("posts/<slug:slug>", views.post_datails,
+         name="post-datails-page")
 ]
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from datetime import date
+
+posts = [
+    {
+        "slug": "hihking in the mountais",
+        "image": "montains.jpg",
+        "auth": "lucasoliveira",
+        "date": date(2024, 9, 28),
+        "title": "Mountain hiking",
+        "excert": "ksdnfkjsnfç çsdfçslkdf lsdfjlksdfml ldkjfklsdfmsdjfkls jksdfklsjdflksjd lksjdfhewp9qunfpiq efuhqweopfjwperfnowf efp9wjef0jef=0",
+        "content": "lore asjdoifplaslore asjdoifplas çaksldjf quepn q9w8hefpqwo p9mclpkd fpodmcwp ecnjffpioqh9p qp9ueiownk apokdcclksiui iwquhoaipqá oijpjifaçaodqp akjnp fponapkldlore asjdoifplas çaksldjf quepn q9w8hefpqwo p9mclpkd fpodmcwp ecnjffpioqh9p qp9ueiownk apokdcclksiui iwquhoaipqá oijpjifaçaodqp akjnp fponapkld çaksldjf quepn q9w8hefpqwo p9mclpkd fpodmcwp ecnjffpioqh9p qp9ueiownk apokdcclksiui iwquhoaipqá oijpjifaçaodqp akjnp fponapkld"
+    }
+]
 
 # Create your views here.
 
@@ -8,5 +21,5 @@ def home_page(request):
 def posts(request):
     return render(request, "blog/all-posts.html")
 
-def post_datails(request):
-    pass
+def post_datails(request, slug):
+    return render(request, "blog/post-datail.html")
